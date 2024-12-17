e4a00ac61e7db28566372fb132e7823a690aec54
Maryam Medjahed <maryam@MacBook-Pro-de-Maryam.local>
2024-12-17 21:53:50 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+# Ta clé API NewsAPI
+NEWS_API_KEY = "6de822b5d43943f280de002acc208fcf"
+
+@app.route("/get-news", methods=["GET"])
+def get_news():
+    country = request.args.get("country", "us")  # Par défaut, "fr" pour la France
+    try:
+        # Appel à l'API NewsAPI
+        url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={NEWS_API_KEY}"
+        response = requests.get(url)
+
+        # Vérifie si la requête est réussie
+        if response.status_code == 200:
+            return jsonify(response.json())
+        else:
+            return jsonify({"error": "Failed to fetch news", "status_code": response.status_code}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
